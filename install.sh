fe28374160215d367308c0b3bec7cf9660f2f5a3
Zhangben Wu <dupidog@gmail.com>
2021-09-07 16:19:56 +0800

    Add _git-completion.bash

--- install.sh
+++ install.sh
@@ -3,6 +3,17 @@
 cp _vimrc ~/.vimrc
 cp _screenrc ~/.screenrc
 cp _gitconfig ~/.gitconfig
+cp _git-completion.bash ~/.git-completion.bash
 
 mkdir -p ~/.vim/plugin
 cp _vim/plugin/* ~/.vim/plugin
+
+if [ -z "`grep '\.git-completion\.bash' ~/.bashrc`" ]; then
+cat >> ~/.bashrc <<EOF
+
+# Add git completion script
+if [ -f ~/.git-completion.bash ]; then 
+  . ~/.git-completion.bash 
+fi 
+EOF
+fi
