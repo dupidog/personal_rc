d290293762f15104130cb50a7329af7948d4f6e1
Zhangben Wu <dupidog@gmail.com>
2021-09-07 16:32:08 +0800

    Save user.name/user.email in ~/.gitconfig and not overwrite it

--- install.sh
+++ install.sh
@@ -1,12 +1,28 @@
 #! /bin/bash
 
+# vim
 cp _vimrc ~/.vimrc
-cp _screenrc ~/.screenrc
-cp _gitconfig ~/.gitconfig
-cp _git-completion.bash ~/.git-completion.bash
 
 mkdir -p ~/.vim/plugin
 cp _vim/plugin/* ~/.vim/plugin
+
+# screen
+cp _screenrc ~/.screenrc
+
+# git
+GIT_USER_NAME=`git config --global user.name`
+GIT_USER_EMAIL=`git config --global user.email`
+
+cp _gitconfig ~/.gitconfig
+
+if [ ! -z "$GIT_USER_NAME" ]; then
+git config --global user.name "$GIT_USER_NAME"
+fi
+if [ ! -z "$GIT_USER_EMAIL" ]; then
+git config --global user.email "$GIT_USER_EMAIL"
+fi
+
+cp _git-completion.bash ~/.git-completion.bash
 
 if [ -z "`grep '\.git-completion\.bash' ~/.bashrc`" ]; then
 cat >> ~/.bashrc <<EOF
@@ -17,3 +33,4 @@ if [ -f ~/.git-completion.bash ]; then
 fi 
 EOF
 fi
+
