97e86957b2c94e235c1ea36596e027a8674c7735
Zhangben Wu <dupidog@gmail.com>
2021-09-24 00:26:20 +0800

    Add bash-completion to install.sh

--- install.sh
+++ install.sh
@@ -28,9 +28,36 @@ if [ -z "`grep '\.git-completion\.bash' ~/.bashrc`" ]; then
 cat >> ~/.bashrc <<EOF
 
 # Add git completion script
-if [ -f ~/.git-completion.bash ]; then 
-  . ~/.git-completion.bash 
-fi 
+if [ -f ~/.git-completion.bash ]; then
+  . ~/.git-completion.bash
+fi
 EOF
 fi
 
+# bash-completion
+BASH_COMPLETION_INSTALLED=`apt list bash-completion 2>/dev/null | grep -o 'installed'`
+if [ -z "$BASH_COMPLETION_INSTALLED" ]; then
+read -p "Package bash-completion not installed, install it? [y/N] " PROMPT
+if [ "$PROMPT" = "y" -o "$PROMPT" = "Y" ]; then
+sudo apt-get install bash-completion -y
+if [ $? -ne 0 ]; then
+echo "Installation failed, check source and run 'apt-get update'"
+elif [ -z "`grep '/etc/bash_completion' ~/.bashrc`" ]; then
+cat >> ~/.bashrc <<EOF
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+EOF
+fi
+else
+echo "Installation aborted."
+fi
+fi
